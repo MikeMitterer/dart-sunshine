9031e4e32021b77aacbb677f7c54a02e0c533f56
Mike Mitterer <office@mikemitterer.at>
2017-04-07 13:50:52 +0200

    Views added, Main-Navigation works

---
+++ lib/components/Forecast.dart
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) 2017, Michael Mitterer (office@mikemitterer.at),
+ * IT-Consulting and Development Limited.
+ * 
+ * All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+     
+part of dart_sunshine.components;
+
+/* 
+/// Basic DI configuration for [Forecast]
+///
+/// Usage:
+///     class MainModule extends di.Module {
+///         MainModule() {
+///             install(new ForecastModule());
+///         }     
+///     }
+class ForecastModule  extends di.Module {
+    ForecastModule() {
+        // bind(DeviceProxy);
+        
+        // -- services
+        // bind(SignalService, toImplementation: SignalServiceImpl);
+    }
+} 
+*/
+
+/// Controller-View for <forecast></forecast>
+///
+@MdlComponentModel
+class Forecast extends MdlComponent {
+    final Logger _logger = new Logger('dart_sunshine.components.Forecast');
+
+    //static const _ForecastConstant _constant = const _ForecastConstant();
+    static const _ForecastCssClasses _cssClasses = const _ForecastCssClasses();
+    
+    /// Change this to a more specific version
+    /* final */ DataStore _store;
+    
+    Forecast.fromElement(final dom.HtmlElement element,final di.Injector injector)
+        : /* _store = injector.get(SpecificDataStore), */ super(element,injector) {
+        
+        _init();
+        
+    }
+    
+    static Forecast widget(final dom.HtmlElement element) => mdlComponent(element,Forecast) as Forecast;
+    
+    // Central Element - by default this is where forecast can be found (element)
+    // html.Element get hub => inputElement;
+    
+    // - EventHandler -----------------------------------------------------------------------------
+
+    void handleButtonClick() {
+        _logger.info("Event: handleButtonClick");
+    }    
+    
+    //- private -----------------------------------------------------------------------------------
+
+    void _init() {
+        _logger.info("Forecast - init");
+        
+        // Recommended - add SELECTOR as class if this component is a TAG!
+        element.classes.add(_ForecastConstant.WIDGET_SELECTOR);
+        
+        final dom.DivElement sample = new dom.DivElement();
+        sample.text = "Your Forecast-Component works!";
+        element.append(sample);
+        
+        _bindStoreActions();
+        _bindViewActions();
+        
+        element.classes.add(_cssClasses.IS_UPGRADED);
+    }
+    
+    /// After the template is rendered we bind all the necessary Actions for this component
+    void _bindStoreActions() {
+        // only after creation...
+        if(_store == null) { return;}
+
+        _store.onChange.listen((final DataStoreChangedEvent event) {
+
+            // Handle specific Update-Actions
+            // if(event.data.actionname == UpdateTimeView.NAME) {
+            //
+            // }
+
+            _updateView();
+
+        });
+    }
+
+    /// Attach Actions coming from our view (HTML-Template)
+    void _bindViewActions() {
+        
+        // final MaterialFormComponent form = MaterialFormComponent.widget(query(".mdl-form"));
+        //
+        // eventStreams.add(
+        //     form.onChange.listen((_) {
+        //         // Create new Person - we don't want to edit the "Store person"!
+        //         // final Person person = _store.byId(id);
+        //
+        //         // _store.fire(new PersonChangedAction(person));
+        //     }));
+    }
+    
+    /// Something has changed in the attached store - visualize it
+    ///
+    /// Usually this function is called if we get an onChange-event from our store
+    void _updateView() {
+        
+    }    
+}
+
+/// Registers the Forecast-Component
+///
+///     main() {
+///         registerForecast();
+///         ...
+///     }
+///
+void registerForecast() {
+    final MdlConfig config = new MdlWidgetConfig<Forecast>(
+        _ForecastConstant.WIDGET_SELECTOR,
+            (final dom.HtmlElement element,final di.Injector injector) => new Forecast.fromElement(element,injector)
+    );
+    
+    // If you want <forecast></forecast> set selectorType to SelectorType.TAG.
+    // If you want <div forecast></div> set selectorType to SelectorType.ATTRIBUTE.
+    // By default it's used as a class name. (<div class="forecast"></div>)
+    config.selectorType = SelectorType.TAG;
+    
+    componentHandler().register(config);
+}
+
+//- private Classes ----------------------------------------------------------------------------------------------------
+
+/// Store strings for class names defined by this component that are used in
+/// Dart. This allows us to simply change it in one place should we
+/// decide to modify at a later date.
+class _ForecastCssClasses {
+
+    final String IS_UPGRADED = 'is-upgraded';
+    
+    const _ForecastCssClasses(); }
+    
+/// Store constants in one place so they can be updated easily.
+class _ForecastConstant {
+
+    static const String WIDGET_SELECTOR = "forecast";
+
+    const _ForecastConstant();
+}  
--- web/main.dart
+++ web/main.dart
@@ -1,11 +1,13 @@
-library mini_webapp.main;
+library dart_sunshine.main;
 
 import 'dart:async';
-import 'dart:html';
+import 'dart:html' as dom;
 
 import 'package:logging/logging.dart';
 import 'package:console_log_handler/console_log_handler.dart';
 import 'package:di/di.dart' as di;
+import 'package:route_hierarchical/client.dart';
+import 'package:validate/validate.dart';
 
 import 'package:mdl/mdl.dart';
 
@@ -16,6 +18,7 @@ final Logger _logger = new Logger('mini_webapp.main');
 @MdlComponentModel @di.Injectable()
 class Application implements MaterialApplication {
     final Logger _logger = new Logger('main.Application');
+    final Router _router = new Router(useFragment: true);
 
     /// Added by the MDL/Dart-Framework (mdlapplication.dart)
     final ActionBus _actionbus;
@@ -26,6 +29,10 @@ class Application implements MaterialApplication {
     @override
     void run() {
         _bindSignals();
+    }
+
+    void go(final String routePath, final Map params) {
+        _router.go(routePath,params);
     }
 
     //- private -----------------------------------------------------------------------------------
@@ -43,37 +50,37 @@ Future main() async {
 
     registerMdl();
 
-    final MaterialApplication application = await componentFactory().rootContext(Application)
+    final Application application = await componentFactory().rootContext(Application)
         .addModule(new SampleModule())
         .run();
 
-    application.run();
-    
-    querySelector('#output').text = "Your Dart app is running.";
-
-    if (sw.isNotSupported) {
-        _logger.warning('ServiceWorkers are not supported.');
-        return;
-    }
-
-    await sw.register('sw.dart.js');
-    _logger.info('registered');
-
-    sw.ServiceWorkerRegistration registration = await sw.ready;
-    _logger.info('ready');
-
-    sw.onMessage.listen((MessageEvent event) {
-        _logger.info('reply received: ${event.data}');
+    configRouter(application._router,(final RoutePreEnterEvent event) {
+        // new Future<bool>(() => application.isUserLoggedIn)
+        event.allowEnter(new Future(() => true));
     });
 
-    sw.ServiceWorker active = registration.active;
-    active.postMessage('x');
-    _logger.info('sent');
+    application.run();
+    
+    if (sw.isSupported) {
+        await sw.register('sw.dart.js');
+        _logger.info('ServiceWorker - registered...');
 
-//    sw.PushSubscription subs = await registration.pushManager
-//        .subscribe(new sw.PushSubscriptionOptions(userVisibleOnly: true));
+        sw.ServiceWorkerRegistration registration = await sw.ready;
+        _logger.info('ServiceWorker - ready!');
 
-//    _logger.fine('endpoint: ${subs.endpoint}');
+        sw.onMessage.listen((dom.MessageEvent event) {
+            _logger.info('reply received: ${event.data}');
+        });
+
+        sw.ServiceWorker active = registration.active;
+        active.postMessage('x');
+        _logger.info('sent');
+
+        //    sw.PushSubscription subs = await registration.pushManager
+        //        .subscribe(new sw.PushSubscriptionOptions(userVisibleOnly: true));
+
+        //    _logger.fine('endpoint: ${subs.endpoint}');
+    }
 }
 
 /**
@@ -84,6 +91,112 @@ class SampleModule extends di.Module {
 
     }
 }
+
+/// Default Controller!!!
+class DefaultController extends MaterialController {
+    static final Logger _logger = new Logger('dart_sunshine.main.DefaultController');
+
+    /// Prefix for [bodyMarker]
+    static const String _VIEW_PREFIX = "view-";
+
+    @override
+    void loaded(final Route route) {
+
+        final Application app = componentFactory().application;
+
+        _logger.info("DefaultController loaded!");
+    }
+    // - private -------------------------------------------------------------------------------------------------------
+
+    void set bodyMarker(final String marker) {
+        Validate.notBlank(marker);
+
+        final dom.HtmlElement body = dom.querySelector("body");
+        body.classes.removeWhere((final String name) => name.startsWith(_VIEW_PREFIX));
+        body.classes.add("${_VIEW_PREFIX}${marker}");
+    }
+}
+
+class ControllerHome extends DefaultController {
+    static final Logger _logger = new Logger('dart_sunshine.main.ControllerHome');
+
+    @override
+    void loaded(final Route route) {
+        super.loaded(route);
+        _logger.info("ControllerHome loaded!");
+
+        bodyMarker = "home";
+
+    }
+
+    @override
+    void unload() {
+        _logger.info("ControllerHome unloaded!");
+
+    }
+    // - private ------------------------------------------------------------------------------------------------------
+}
+
+class ControllerSettings extends DefaultController {
+    static final Logger _logger = new Logger('dart_sunshine.main.ControllerSettings');
+
+    @override
+    void loaded(final Route route) {
+        super.loaded(route);
+        _logger.info("ControllerSettings loaded!");
+
+        bodyMarker = "settings";
+    }
+
+    @override
+    void unload() {
+        _logger.info("ControllerSettings unloaded!");
+
+    }
+    // - private ------------------------------------------------------------------------------------------------------
+}
+
+class ControllerMap extends DefaultController {
+    static final Logger _logger = new Logger('dart_sunshine.main.ControllerMap');
+
+    @override
+    void loaded(final Route route) {
+        super.loaded(route);
+        _logger.info("ControllerMap loaded!");
+
+        bodyMarker = "map";
+    }
+
+    @override
+    void unload() {
+        _logger.info("ControllerMap unloaded!");
+
+    }
+    // - private ------------------------------------------------------------------------------------------------------
+}
+
+void configRouter(final Router router,final RoutePreEnterEventHandler routeChecker) {
+    final ViewFactory view = new ViewFactory();
+
+    router.root
+
+        ..addRoute(name: 'settings', path: '/settings',
+            enter: view("views/settings.html", new ControllerSettings()),
+            preEnter: routeChecker)
+
+        ..addRoute(name: 'map', path: '/map',
+            enter: view("views/map.html", new ControllerMap()),
+            preEnter: routeChecker)
+
+        /// Leave default-route as the last one
+        ..addRoute(name: 'home', defaultRoute: true, path: '/',
+            enter: view("views/home.html", new ControllerHome()))
+
+    ;
+
+    router.listen();
+}
+
 void configLogger() {
     hierarchicalLoggingEnabled = false; // set this to true - its part of Logging SDK
 
